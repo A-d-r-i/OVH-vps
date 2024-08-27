40b09c81213a72ca97ba374f66a955b8f367cc43
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2024-08-27 11:44:22 +0200

    Update ovhvps.ps1

--- ovhvps.ps1
+++ ovhvps.ps1
@@ -4,6 +4,7 @@ $chatid = "$env:CHAT_ID"
 
 $data = ((Invoke-Webrequest $link | ConvertFrom-Json).datacenters | Where-Object { $_.datacenter -in @("SBG", "GRA")})
 $dispo = $data | Where-Object {$_.status -eq "available"} #out-of-stock
+
 if ($dispo -eq $NULL) {
 	
 	"VPS indisponible"
@@ -18,17 +19,18 @@ if ($dispo -eq $NULL) {
 		$message += "➔ <a href=`"$link`">$name</a> `n"
 	
 	}
+
+	$url = "https://api.telegram.org/bot$token/sendMessage"
+
+	$parameters = @{
+		chat_id = $chatid
+		text = $message
+		parse_mode = "HTML"
+	}
+
+	$json = $parameters | ConvertTo-Json -Compress
+	$utf8_txt = [System.Text.Encoding]::UTF8.GetBytes($json)
+
+	Invoke-RestMethod -Uri $url -Method Post -ContentType "application/json" -Body $utf8_txt
+
 }
-
-$url = "https://api.telegram.org/bot$token/sendMessage"
-
-$parameters = @{
-	chat_id = $chatid
-	text = $message
-	parse_mode = "HTML"
-}
-
-$json = $parameters | ConvertTo-Json -Compress
-$utf8_txt = [System.Text.Encoding]::UTF8.GetBytes($json)
-
-Invoke-RestMethod -Uri $url -Method Post -ContentType "application/json" -Body $utf8_txt
