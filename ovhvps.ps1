6ac600b29745d52f22f35e9c3d3fb1b4435e541c
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2024-08-27 11:17:38 +0200

    Create ovhvps.ps1

---
+++ ovhvps.ps1
@@ -0,0 +1,23 @@
+$link = "$env:URL"
+
+$data = ((Invoke-Webrequest $link | ConvertFrom-Json).datacenters | Where-Object { $_.datacenter -in @("SBG", "GRA")})
+$dispo = $data | Where-Object {$_.status -eq "out-of-stock"}
+
+if ($dispo -eq $NULL) {
+	
+	"VPS indisponible"
+	
+} else {
+	$message = "⚠ *VPS VLE\-4 DISPO \!* `n"
+	
+	foreach($datacenter in $dispo) {
+		$name = $datacenter.datacenter
+		$link = "https://www.ovh.com/fr/order/vps/"
+		$message += "➔ [$name]($link) `n"
+	}
+}
+
+
+$token = "$env:TELEGRAM"
+$chatid = "$env:CHAT_ID"
+Invoke-RestMethod -Uri "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid&parse_mode=MarkdownV2&text=$message"
